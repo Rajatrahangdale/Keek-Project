13858b77db0c2c6ba86d0cc77f87d7d26cfaad51
Rajat Rahangdale <rjtrahangdale@gmail.com>
2024-09-10 15:22:42 +0530

    Favorite component functinality update

--- src/Manage/DiscoverInfluencers/Card.jsx
+++ src/Manage/DiscoverInfluencers/Card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useContext } from "react";
 import { MdVerified } from "react-icons/md";
 import { IoMdStar } from "react-icons/io";
--- src/Manage/DiscoverInfluencers/Favorites.jsx
+++ src/Manage/DiscoverInfluencers/Favorites.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useRef, useContext } from "react";
 import { GoKebabHorizontal } from "react-icons/go";
 import { FaHeart, FaPlus } from "react-icons/fa6";
